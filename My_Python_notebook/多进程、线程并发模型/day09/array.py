41b361eed4d181e5bc83cb1155ef19c6a4b67b08
Home_ZYBB <18210234186@163.com>
2023-04-25 13:55:42 +0800

    xxx

--- My_Python_notebook/多进程、线程并发模型/day09/array.py
+++ My_Python_notebook/多进程、线程并发模型/day09/array.py
@@ -8,16 +8,19 @@ from multiprocessing import Process,Array
 # shm = Array('i',[1,2,3,4])
 # shm = Array('i',4) # 开辟4个整形的列表空间
 shm = Array('c',b'hello')
-
+print(shm[0])
 def fun():
     # 共享内存对象可以迭代
     for i in shm:
         print(i)
+    print(shm)
     shm[0] = b'H' # 修改共享内存
+    print('shm[0]:',shm[0])
+if __name__ == '__main__':
 
-p = Process(target=fun)
-p.start()
-p.join()
-for i in shm:
-    print(i)
-print(shm.value) # 整体打印字节串
+    p = Process(target=fun)
+    p.start()
+    p.join()
+    for i in shm:
+        print(i)
+    print(shm[0]) # 整体打印字节串
